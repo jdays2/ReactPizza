bcb9242e434198097c06cbd1010aab845fb28db2
MarkHW <lolka-pop@rambler.ru>
2023-02-02 16:22:02 +0300

    folders menagering

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import s from "./Menu.module.css";
+import PizzaBlock from "./PizzaBlock/PizzaBlock";
+
+function Menu() {
+  return (
+    <div className="content__items">
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+      <PizzaBlock />
+    </div>
+  );
+}
+
+export default Menu;
