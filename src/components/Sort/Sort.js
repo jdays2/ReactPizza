17d8ab87b9670b02af0e1801573d1644c053c295
MarkHW <lolka-pop@rambler.ru>
2023-02-04 15:28:43 +0300

    create working popup, using map, key and refact

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,4 +1,16 @@
+import React from "react";
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const sortList = ["популярности", "цене", "алфавиту"];
+  const [activeList, setActiveList] = React.useState(0);
+  const selectedList = sortList[activeList];
+
+  const closeSelectedList = (s, i) => {
+    setActiveList(i);
+    setOpen(false);
+  };
+
   return (
     <div className="sort">
       <div className="sort__label">
@@ -15,15 +27,31 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {selectedList}
+        </span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sortList.map((s, i) => (
+              <li
+                className={activeList === i ? "active" : ""}
+                key={i}
+                onClick={() => {
+                  closeSelectedList(s, i);
+                }}
+              >
+                {s}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
