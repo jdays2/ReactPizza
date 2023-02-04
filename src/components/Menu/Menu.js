a248d65be48e89fc6ab1e7062b621615aa7eddda
MarkHW <lolka-pop@rambler.ru>
2023-02-04 16:00:23 +0300

    fetch('https://63de555b9fa0d60060fce0cd.mockapi.io/api/')

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,11 +1,18 @@
 import s from "./Menu.module.css";
 import PizzaBlock from "./PizzaBlock/PizzaBlock";
-import pizzas from "../../assets/pizzas.json";
+
+import React from "react";
 
 function Menu() {
+  const [data, setData] = React.useState([]);
+
+  fetch("https://63de555b9fa0d60060fce0cd.mockapi.io/api/items")
+    .then((response) => response.json())
+    .then((json) => setData(json));
+
   return (
     <div className="content__items">
-      {pizzas.map((p) => {
+      {data.map((p) => {
         return <PizzaBlock {...p} key={p.id} />;
       })}
     </div>
