6eabbe3499185245b48c5d327682c508ac7c9a61
MarkHW <lolka-pop@rambler.ru>
2023-02-05 03:53:52 +0300

    add skeleton befor fetch

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,22 +1,30 @@
 import s from "./Menu.module.css";
 import PizzaBlock from "./PizzaBlock/PizzaBlock";
+import PizzaBlockSkeleton from "./PizzaBlock/PizzaBlockSkeleton";
 
 import React from "react";
 
 function Menu() {
   const [data, setData] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://63de555b9fa0d60060fce0cd.mockapi.io/api/items")
-      .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((data) => data.json())
+      .then((state) => {
+        setIsLoading(false);
+        setData(state);
+      });
   }, []);
 
   return (
     <div className="content__items">
-      {data.map((p) => {
+      {isLoading
+        ? [...new Array(6)].map((_, i) => <PizzaBlockSkeleton key={i} />)
+        : data.map((p) => <PizzaBlock {...p} key={p.id} />)}
+      {/* {data.map((p) => {
         return <PizzaBlock {...p} key={p.id} />;
-      })}
+      })} */}
     </div>
   );
 }
