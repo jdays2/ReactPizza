d4e9ee4963be15ba84741537e816f95292d1faf8
MarkHW <lolka-pop@rambler.ru>
2023-02-09 10:07:35 +0300

    проблемы с debouce и useCallback.Решу после работы

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -4,11 +4,22 @@ import closeIcon from "../../../assets/img/close-icon.svg";
 import React from "react";
 import { setCurrentValue } from "./../../../redux/slices/SearchSlice";
 import { useSelector, useDispatch } from "react-redux";
+import debounce from "lodash.debounce";
 
 function Search() {
   const dispatch = useDispatch();
   const currentValue = useSelector((state) => state.search.currentValue);
   const importRef = React.useRef();
+
+  const console = console.log("hello");
+
+  const testDeb = _.debounce(console, 500);
+
+  const onHandle = (event) => {
+    dispatch(setCurrentValue(event.target.value));
+    importRef.current.focus();
+    testDeb();
+  };
 
   return (
     <div className={styles.root}>
@@ -18,8 +29,7 @@ function Search() {
         className={styles.search}
         placeholder="Поиск любимой пиццы"
         onChange={(event) => {
-          dispatch(setCurrentValue(event.target.value));
-          importRef.current.focus();
+          onHandle(event);
         }}
         value={currentValue}
       />
